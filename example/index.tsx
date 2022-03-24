d64473af40334f83d7cb40298fa15d321b8fed00
leoor22 <leonelrivas.rl@gmail.com>
2022-03-23 21:23:22 -0300

    version 1.0.0-rc ready

--- example/index.tsx
+++ example/index.tsx
@@ -5,7 +5,7 @@ import { ProductCard, ProductImage, ProductTitle, ProductButtons } from '../.';
 
 const product = {
   id: '1',
-  title: 'Coffee Mug - Meme',
+  title: 'Coffee Mug - Meme !',
   // img: "./coffee-mug2.png  ",
 };
 
